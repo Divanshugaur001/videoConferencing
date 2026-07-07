ad15b57f83ecb2e8a6b46efc3733de7e86254b93
divanshu <divanshugaur001@gmail.com>
2026-07-07 08:13:46 +0530

    x

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,39 @@
 import express from "express";
-import {createServer} from "node:http";
+import { createServer } from "node:http";
+
 import { Server } from "socket.io";
+
 import mongoose from "mongoose";
-import cors from "cors";
 import { connectToSocket } from "./controllers/socketManager.js";
-import  userRoute  from "./routes/user.routes.js";
 
- const app=express();
- const server= createServer(app);
- const io=connectToSocket(server);
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
- app.set('port',(process.env.PORT||8000));
- app.use(cors());
- app.use(express.json({limit:'40kb'}));
- app.use(express.urlencoded({limit:"40kb",extended:true}));
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
- app.get("/home",(req,res)=>{
-return res.json({"hello ":"world"});
- });
- app.use("/api/v1/users",userRoute);
 
- const start=async()=>{
-    const connectionDb= await mongoose.connect("mongodb+srv://divanshugaur001_db_user:Divanshu001@cluster0.asswdgm.mongodb.net/video?appName=Cluster0")
-    console.log(`mongo connected`);
-    server.listen(app.get("port"),()=>{
-        console.log("listen  on port 8000");
-     });
- }
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
- start();
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
+
+
+}
+
+
+
+start();
